0451984688038a679d26e7c84f650947b872d717
Estifanos <estifanosbeyene21@gmail.com>
2022-12-04 19:19:36 -0500

    feat: pass tests, change func names

--- day4/script.js
+++ day4/script.js
@@ -5,44 +5,51 @@
 export const getNumArr = (str) => str.split("-").map((el) => Number(el));
 
 /**
- * @param {number[][][]} arrs
- * @description return all the numbers in a range
- */
-const getAllNumRange = (arrs) =>
-  arrs.map((arr) =>
-    arr.map((el) => [...Array(el[1] - el[0] + 1).keys()].map((x) => x + el[0]))
-  );
-
-// overlap function for question 1
-export const isOverlap = (arr1, arr2) =>
-  arr1.every((element) => arr2.indexOf(element) !== -1) ||
-  arr2.every((element) => arr1.indexOf(element) !== -1);
-
-// overlap function for question 2
-export const isOverlap2 = (arr1, arr2) =>
-  arr1.some((element) => arr2.indexOf(element) !== -1) ||
-  arr2.some((element) => arr1.indexOf(element) !== -1);
-
-/**
- * @param {[number[], number[]][]} arr
- * @description get sum of the total arrays that overlap
- */
-export const getOverlapBoolArr = (arr) =>
-  arr.map((el) => isOverlap(el[0], el[1]));
-
-/**
- * @param {boolean[]} arr
- * @description returns the total true count
- */
-export const getTrueCount = (arr) =>
-  arr.reduce((prev, curr) => (curr ? prev + 1 : prev), 0);
-
-/**
  * @param {string[][]} arrs
  * @description return array of num
  */
-export const getArrofNumArr = (arrs) =>
+export const getNumArrs = (arrs) =>
   arrs.map((arr) => arr.map((str) => getNumArr(str)));
 
-export const getOverlapCountTotal = (arr) =>
-  getTrueCount(getOverlapBoolArr(getAllNumRange(getArrofNumArr(arr))));
+/**
+ * @param {number} start
+ * @param {number} end
+ * @description return all the numbers in a range
+ */
+export const getRange = ([start, end]) =>
+  [...Array(end - start + 1).keys()].map((x) => x + start);
+
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @description overlap for question 1
+ */
+export const isOverlap = ([arr1, arr2]) =>
+  getRange(arr1).every((element) => getRange(arr2).includes(element)) ||
+  getRange(arr2).every((element) => getRange(arr1).includes(element));
+
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @description overlap for question 2
+ */
+export const isOverlap2 = ([arr1, arr2]) =>
+  getRange(arr1).some((element) => getRange(arr2).includes(element)) ||
+  getRange(arr2).some((element) => getRange(arr1).includes(element));
+
+/**
+ * @param {[number[], number[]][]} arrs
+ * @description get sum of the total arrays that overlap
+ */
+export const getOverlapCount = (arrs, question2) =>
+  question2
+    ? arrs.map((arr) => isOverlap2(arr)).filter((el) => el === true).length
+    : arrs.map((arr) => isOverlap(arr)).filter((el) => el === true).length;
+
+/**
+ * @param {string[][]} arr
+ * @param {boolean} question2
+ * @description get answer for question 1 or question 2
+ */
+export const getOverlapCountTotal = (arr, question2 = false) =>
+  getOverlapCount(getNumArrs(arr), question2);
--- day4/script.test.js
+++ day4/script.test.js
@@ -2,9 +2,85 @@ import { test } from "tap";
 import fs from "fs";
 import path from "path";
 
-import { getOverlapCountTotal } from "./script.js";
+import {
+  isOverlap,
+  getNumArr,
+  getNumArrs,
+  getOverlapCountTotal,
+  getRange,
+} from "./script.js";
 
-test("expect the answer of question 1", async ({ equal }) => {
+test("Testing for first question", async ({ test }) => {
+  test("get range given endpoints", async ({ same }) => {
+    same(getRange([2, 5]), [2, 3, 4, 5]);
+  });
+
+  test("change str to num arr", async ({ same }) => {
+    same(getNumArr("2-4"), [2, 4], "Get num arr");
+    same(getNumArr("6-8"), [6, 8], "Get num arr");
+  });
+
+  test("check if overlap", async ({ equal }) => {
+    const given = [
+      [1, 3],
+      [1, 6],
+    ];
+    equal(isOverlap(given), true);
+  });
+
+  test("get range", async ({ same }) => {
+    const given = [1, 3];
+    const expected = [1, 2, 3];
+    same(getRange(given), expected, "get range 1-3");
+  });
+
+  test("arr of string to arr of number arr", async ({ same }) => {
+    const given = [
+      ["1-3", "3-4"],
+      ["1-3", "1-6"],
+    ];
+    const expected = [
+      [
+        [1, 3],
+        [3, 4],
+      ],
+      [
+        [1, 3],
+        [1, 6],
+      ],
+    ];
+
+    same(getNumArrs(given), expected);
+  });
+
+  test("expect the answer for sample data", async ({ equal }) => {
+    const given = [
+      ["2-4", "6-8"],
+      ["2-3", "4-5"],
+      ["5-7", "7-9"],
+      ["2-8", "3-7"],
+      ["6-6", "4-6"],
+      ["2-6", "4-8"],
+    ];
+    const expected = 2;
+    equal(getOverlapCountTotal(given), expected);
+  });
+
+  test("expect the answer of question 1", async ({ equal }) => {
+    const filePath = path.join(process.cwd(), "day4", "input");
+    const fileContent = fs
+      .readFileSync(filePath, "utf8")
+      .toString()
+      .split("\n")
+      .map((el) => el.split(","))
+      .slice(0, -1);
+    const given = getOverlapCountTotal(fileContent);
+    const expected = 509;
+    equal(given, expected);
+  });
+});
+
+test("expect the answer of question 2", async ({ equal }) => {
   const filePath = path.join(process.cwd(), "day4", "input");
   const fileContent = fs
     .readFileSync(filePath, "utf8")
@@ -12,7 +88,7 @@ test("expect the answer of question 1", async ({ equal }) => {
     .split("\n")
     .map((el) => el.split(","))
     .slice(0, -1);
-  const given = getOverlapCountTotal(fileContent);
-  const expected = 509;
+  const given = getOverlapCountTotal(fileContent, true);
+  const expected = 870;
   equal(given, expected);
 });
